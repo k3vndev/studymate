0978d02b7ebb814d3ecb91b20c38c4bd7b47879d
K3vnDev <kevinrpolo16@gmail.com>
2026-05-11 17:04:18 -0500

    Add new section with bento style grid images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,28 +16,21 @@ export default async function Home() {
   if (user !== null) redirect('/dashboard')
 
   const bullets = [
-    {
-      header: 'Random Tutorials',
-      kicker: 'Consuming content ≠ making progress.'
-    },
-    {
-      header: 'No Structure',
-      kicker: 'Learning feels impossible when every path is improvised.'
-    },
-    {
-      header: 'No accountability',
-      kicker: 'Without progress tracking, motivation disappears fast.'
-    }
+    { header: 'Random Tutorials', kicker: 'Consuming content ≠ making progress.' },
+    { header: 'No Structure', kicker: 'Learning feels impossible when every path is improvised.' },
+    { header: 'No accountability', kicker: 'Without progress tracking, motivation disappears fast.' }
   ]
+
+  const gridImages = ['dashboard', 'studyplan', 'profile']
 
   return (
     <>
       <main
-        className={`w-full flex flex-col items-center pt-20 pb-32 max-w-5xl mx-auto [&>section]:w-full [&>section]:flex [&>section]:flex-col [&>section]:items-center [&>section]:justify-center [&>section]:gap-10 gap-28 ${FONTS.POPPINS}`}
+        className={`w-full flex flex-col items-center py-24 max-w-5xl mx-auto [&>section]:w-full [&>section]:flex [&>section]:flex-col [&>section]:items-center [&>section]:justify-center [&>section]:gap-10 gap-28 ${FONTS.POPPINS}`}
       >
-        <section>
-          <div className='text-center mt-8'>
-            <h1 className='landing-gradient bg-clip-text text-transparent leading-[1.15] font-extrabold text-7xl py-4 mb-4'>
+        <section className='text-center'>
+          <div>
+            <h1 className='landing-gradient bg-clip-text text-transparent leading-[1.15] font-extrabold text-7xl pb-2 mb-4'>
               Stop just trying to study. Start finishing Studyplans.
             </h1>
             <h2 className='text-3xl text-gray-10 font-semibold'>
@@ -52,7 +45,7 @@ export default async function Home() {
               alt='Screenshot of the Studymate chat interface, showing a conversation with Mate, the study assistant and a Studyplan being generated'
               width={1200}
               height={700}
-              className='border border-gray-30 rounded-[2rem] border-t-white/20 border-t-2 h-96 w-full object-cover'
+              className='border border-gray-30 rounded-[2rem] border-t-white/15 border-t-2 h-96 w-full object-cover'
               style={{ maskImage: 'linear-gradient(to bottom, white, transparent)' }}
               draggable={false}
             />
@@ -67,7 +60,7 @@ export default async function Home() {
         <section>
           <h3 className='text-5xl font-bold text-white'>Why Most Self-Learning Fails?</h3>
 
-          <article className='flex items-center gap-8 bg-gradient-to-b from-purple-700/10 to-white/[0.025] px-32 py-16 border border-white/10 rounded-[2rem]'>
+          <article className='flex items-center gap-8 bg-gradient-to-b from-purple-700/10 to-white/[0.025] px-32 py-16 border border-white/10 rounded-[2rem] border-t-white/15 border-t-2'>
             <Image
               src='/mate/sitting.webp'
               alt='Mate sitting in the floor, looking away thoughtfully'
@@ -83,6 +76,26 @@ export default async function Home() {
               ))}
             </ul>
           </article>
+        </section>
+
+        <section className='text-center'>
+          <h2 className='landing-gradient bg-clip-text text-transparent leading-[1.15] font-extrabold text-5xl max-w-[55rem] pb-1 text-center'>
+            Studymate turns chaotic learning into guided progression.
+          </h2>
+
+          <div className='grid grid-cols-2 gap-4'>
+            {gridImages.map(image => (
+              <Image
+                key={image}
+                src={`/screenshots/${image}.webp`}
+                alt={`Screenshot of the Studymate ${image} page`}
+                draggable={false}
+                width={1080}
+                height={1080}
+                className='first:row-span-2 size-full object-cover border border-white/10 rounded-xl first:rounded-l-[2rem] last:rounded-br-[2rem] [&:nth-child(2)]:rounded-tr-[2rem]'
+              />
+            ))}
+          </div>
         </section>
       </main>
 
